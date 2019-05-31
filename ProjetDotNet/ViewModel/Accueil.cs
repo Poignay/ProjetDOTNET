b186d4497b853d2da7d245db8fefcafad4491b2b
Sevven45 <benji.esco@hotmail.fr>
2019-05-31 16:17:33 +0200

    Push

--- ProjetDotNet/ViewModel/Accueil.cs
+++ ProjetDotNet/ViewModel/Accueil.cs
@@ -1,74 +1,63 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetDotNet.ViewModel
 {
-    class Accueil
+    class Accueil : Inotifier
     {
         private MainViewModel mvParent;
         public Accueil(MainViewModel parent)
         {
             mvParent = parent;
-        }
-
-        private ObservableCollection<Class.Media> _myOb;
-        public ObservableCollection<Class.Media> myOb
-        {
-            get
-            {
-                LoadData();
-                return _myOb;
-            }
-            set
-            {
-                myOb = _myOb;
-            }
-        }
-        private String _titre;
-        public String Titre
-        {
-            get
-            {
-                return _titre;
-            }
-            set
-            {
-                Titre = _titre;
-            }
+            LoadData();
         }
 
         public int nbrMedia
         {
-            get;
-            set;
+            get
+            {
+
+                return GetValue<int>();
+            }
+            set
+            {
+                SetValue(value);
+            }
         }
 
+        public int nbrFilm
+        {
+            get
+            {
+
+                return GetValue<int>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
 
         public async Task LoadData()
         {
 
             var context = await DataAccess.DbContext.GetCurrent();
 
-            context.Add(new Class.Media()
-            {
-                Titre = "Film1",
-            });
-
-            // Enregistrement
-            await context.SaveChangesAsync();
-
             // Sélection des medias
-            ObservableCollection<Class.Media> myOb = new ObservableCollection<Class.Media>(context.Medias);
+            ObservableCollection<Class.Media> allMedia = new ObservableCollection<Class.Media>(context.Medias.ToList());
 
-            foreach (Class.Media i in myOb)
-            {
-                _titre = i.Titre;
-            }
+            nbrMedia = allMedia.Count;
+
+
+            ObservableCollection<Class.Media> allFilm = new ObservableCollection<Class.Media>(context.Films.ToList());
+
+            nbrFilm = allFilm.Count;
+
 
         }
     }

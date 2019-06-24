3dbce788e45bce52a521925edcd1b11fc2c7545f
Sevven45 <benji.esco@hotmail.fr>
2019-06-24 22:24:18 +0200

    Style, toujours, manque bouton

--- ProjetDotNet/ViewModel/Accueil.cs
+++ ProjetDotNet/ViewModel/Accueil.cs
@@ -45,6 +45,18 @@ namespace ProjetDotNet.ViewModel
             set{SetValue(value);}
         }
 
+        public int nbrVu
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int nbrEnCours
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
         public int nbrDansGenre
         {
             get{return GetValue<int>();}
@@ -114,6 +126,63 @@ namespace ProjetDotNet.ViewModel
             set { SetValue(value); }
         }
 
+        public ChartValues<double> valeurMediaVu
+        {
+            get { return GetValue<ChartValues<double>>(); }
+            set { SetValue(value); }
+        }
+
+        public ChartValues<double> valeurMediaAvoir
+        {
+            get { return GetValue<ChartValues<double>>(); }
+            set { SetValue(value); }
+        }
+
+        public ChartValues<double> valeurMediaEnCours
+        {
+            get { return GetValue<ChartValues<double>>(); }
+            set { SetValue(value); }
+        }
+
+
+        //Note
+        public int note0
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int note1
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int note2
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int note3
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int note4
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        public int note5
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+
         public async Task LoadComboBox()
         {
             var context = await DataAccess.DbContext.GetCurrent();
@@ -133,6 +202,11 @@ namespace ProjetDotNet.ViewModel
         }
 
         public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
 
         public async Task LoadData()
         {
@@ -159,6 +233,15 @@ namespace ProjetDotNet.ViewModel
             ObservableCollection<Class.Media> aVoir = new ObservableCollection<Class.Media>(context.Medias.Where(m=> m.Statut.ToString() == "A_voir").ToList());
 
             nbrAVoir = aVoir.Count;
+
+            //Sélection du nombre de films/Séries vu
+            ObservableCollection<Class.Media> Vu = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Statut.ToString() == "Vu").ToList());
+
+            nbrVu = Vu.Count;
+
+            //Sélection du nombre de films/Séries vu
+            ObservableCollection<Class.Media> EnCours = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Statut.ToString() == "En_cours").ToList());
+            nbrEnCours = EnCours.Count;
 
             // Sélection du nombre de total de média suivant le Genre sélectionné
             ObservableCollection<Class.Media> dansGenre = new ObservableCollection<Class.Media>(context.Medias.Include(m => m.Genres).Where(m => m.Genres.Any(g => g.Genre.nom == typeCBoxSelected)).ToList());
@@ -187,12 +270,62 @@ namespace ProjetDotNet.ViewModel
             }else{
                 nbrNotes = 0;
             }
+
+
             
+            
+
             valeurSerie = new ChartValues<Double> { nbrSerie };
             valeurFilm = new ChartValues<Double> { nbrFilm };
 
             PointLabel = chartPoint =>
                 string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+
+            valeurMediaVu = new ChartValues<Double> { nbrVu };
+            valeurMediaAvoir = new ChartValues<Double> { nbrAVoir };
+            valeurMediaEnCours = new ChartValues<Double> { nbrEnCours };
+           
+
+
+
+            // Sélection du nombre de média par note
+            ObservableCollection<Class.Media> listeNote0 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 0).ToList());
+            note0 = listeNote0.Count;
+
+            ObservableCollection<Class.Media> listeNote1 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 1).ToList());
+            note1 = listeNote1.Count;
+
+            ObservableCollection<Class.Media> listeNote2 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 2).ToList());
+            note2 = listeNote2.Count;
+
+            ObservableCollection<Class.Media> listeNote3 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 3).ToList());
+            note3 = listeNote3.Count;
+
+            ObservableCollection<Class.Media> listeNote4 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 4).ToList());
+            note4 = listeNote4.Count;
+
+            ObservableCollection<Class.Media> listeNote5 = new ObservableCollection<Class.Media>(context.Medias.Where(m => m.Note == 5).ToList());
+            note5 = listeNote5.Count;
+
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Nombre média",
+                    Values = new ChartValues<double> { note0, note1, note2, note3, note4, note5}
+                }
+            };
+
+            
+
+            Labels = new string[] {"0","1","2","3","4","5"};
+
+            
+
+            Formatter = value => value.ToString("N");
+
         }
     }
 }
